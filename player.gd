705e0f4d3c7d2eb7ec4da1cca0b274936396b3b9
Leno Oshiro <leno.oshiro@gmail.com>
2025-07-26 17:38:53 -0300

    Pushing assets from yt tutorial

---
+++ player.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * 600
+	move_and_slide()
+	
+	get_node("HappyBoo").play_walk_animation()
