c374326df7cf7e920c4bf4da20ebb6214e910bee
Leno Oshiro <leno.oshiro@gmail.com>
2025-07-27 11:19:43 -0300

    Adding mobs and making them follow the player

---
+++ mob.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+@onready var player = get_node("/root/Game/Player")
+
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 200
+	move_and_slide()
