05d54602e5af18801de2ff8021458fee736c599c
Leno Oshiro <leno.oshiro@gmail.com>
2025-07-28 20:41:21 -0300

    Adding gun and bullets to player, health and damage to mobs

--- mob.gd
+++ mob.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
+var health = 3
 @onready var player = get_node("/root/Game/Player")
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 200
 	move_and_slide()
+
+func take_damage():
+	health -= 1
+	if health == 0:
+		queue_free()
