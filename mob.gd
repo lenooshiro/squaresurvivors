2ba6d501fdce093af1302a77a3e311ab3154ec6e
Leno Oshiro <leno.oshiro@gmail.com>
2025-07-28 20:48:54 -0300

    Smoke animation when mobs die

--- mob.gd
+++ mob.gd
@@ -16,3 +16,7 @@ func take_damage():
 	$Slime.play_hurt()
 	if health == 0:
 		queue_free()
+		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
