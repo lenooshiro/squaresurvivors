5f00b843f36a6d4eb069a33d6713637ec425d3cf
Leno Oshiro <leno.oshiro@gmail.com>
2025-07-28 20:44:50 -0300

    Walk and hurt animation for mobs

--- mob.gd
+++ mob.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 var health = 3
 @onready var player = get_node("/root/Game/Player")
 
+func _ready():
+	%Slime.play_walk()
+
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 200
@@ -10,5 +13,6 @@ func _physics_process(delta: float) -> void:
 
 func take_damage():
 	health -= 1
+	$Slime.play_hurt()
 	if health == 0:
 		queue_free()
